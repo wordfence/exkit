bee75c3f11719b4d9732e3132791f263208993fe
Ryan Britton <ryan@MacPro.local>
2016-05-06 16:26:52 -0700

    Added a sample PoC file to demonstrate some usage syntax.

---
+++ poc-sample.php
@@ -0,0 +1,19 @@
+#!/usr/bin/env php
+<?php
+
+require 'vendor/autoload.php';
+
+use Wordfence\WPKit\WPAuthentication;
+use Wordfence\WPKit\Cli;
+
+//Prompt for something
+$value = Cli::prompt("Enter a value", 'some default');
+Cli::write('Value: ' . $value);
+
+//Prompt for the baseURL early if needed
+Endpoint::baseURL();
+
+//Log in as a subscriber
+$session = new \Requests_Session();
+WPAuthentication::logInAsUserRole($session, WPAuthentication::USER_ROLE_SUBSCRIBER);
+Cli::write('[+] Logged in', 'green', null);
