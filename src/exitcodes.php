e02bd02a3bee7722a96273a817ce565b082ab471
Ryan Britton <ryan@MacPro.local>
2016-05-06 16:20:52 -0700

    Changed ordering of exit codes. Added more to .gitignore.

--- src/exitcodes.php
+++ src/exitcodes.php
@@ -4,7 +4,7 @@ namespace Wordfence\WPKit;
 
 class ExitCodes
 {
-	const EXIT_CODE_EXPLOIT_FAILED = 0;
-	const EXIT_CODE_EXPLOIT_SUCCEEDED = 1;
+	const EXIT_CODE_EXPLOIT_SUCCEEDED = 0;
+	const EXIT_CODE_EXPLOIT_FAILED = 1;
 	const EXIT_CODE_FAILED_PRECONDITION = 2;
 }
