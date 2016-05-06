6d790b355c0db64e382cc2249aa2edc55095d0fd
Ryan Britton <ryan@MacPro.local>
2016-05-06 14:08:11 -0700

    Added ExitCodes class.

---
+++ src/exitcodes.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace \Wordfence\WPKit;
+
+class ExitCodes
+{
+	const EXIT_CODE_EXPLOIT_FAILED = 0;
+	const EXIT_CODE_EXPLOIT_SUCCEEDED = 1;
+	const EXIT_CODE_FAILED_PRECONDITION = 2;
+}
