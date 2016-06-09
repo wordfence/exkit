691490f24e03fe628d462702c9fc4b5046b3f537
Ryan Britton <ryan@MacPro.local>
2016-06-09 10:33:19 -0700

    Added findAll versions of two functions.

--- src/page.php
+++ src/page.php
@@ -4,8 +4,9 @@ namespace Wordfence\WPKit;
 
 class Page
 {
-	/*
-	 * Retrieves the given $url and returns any capture groups in $regex for the first match or false if not found. The structure of the returned capture groups matches preg_match: http://php.net/manual/en/function.preg-match.php
+	/**
+	 * Retrieves the given $url and returns any capture groups in $regex for the first match or false if not found. The 
+	 * structure of the returned capture groups matches preg_match: http://php.net/manual/en/function.preg-match.php
 	 * 
 	 * @param Requests_Session $session The session to send the request from.
 	 * @param string $url The page URL.
@@ -15,8 +16,29 @@ class Page
 	 */
 	public static function find(\Requests_Session $session, $url, $regex)
 	{
+		$matches = self::findAll($session, $url, $regex);
+		if (is_array($matches)) {
+			if (count($matches)) { return $matches[0]; }
+			return [];
+		}
+		return false;
+	}
+	
+	/**
+	 * Retrieves the given $url and returns any capture groups in $regex for the all matches or false if not found. The 
+	 * structure of the returned capture groups matches preg_match_all: 
+	 * http://php.net/manual/en/function.preg-match-all.php with the flag PREG_SET_ORDER.
+	 * 
+	 * @param Requests_Session $session The session to send the request from.
+	 * @param string $url The page URL.
+	 * @param string $regex The regex to use.
+	 * 
+	 * @return array|false The capture groups if found, otherwise false.
+	 */
+	public static function findAll(\Requests_Session $session, $url, $regex)
+	{
 		$r = $session->get($url);
-		if (preg_match($regex, $r->body, $matches)) {
+		if (preg_match_all($regex, $r->body, $matches, PREG_SET_ORDER)) {
 			return $matches;
 		}
 		return false;
--- src/wpnonce.php
+++ src/wpnonce.php
@@ -4,7 +4,7 @@ namespace Wordfence\WPKit;
 
 class WPNonce
 {
-	/*
+	/**
 	 * Retrieves the given $url and returns the first match for the nonce regex or false if not found.
 	 * 
 	 * @param Requests_Session $session The session to send the request from.
@@ -21,5 +21,28 @@ class WPNonce
 			return false;
 		}
 		return $nonceMatches[1];
+	}
+	
+	/**
+	 * Retrieves the given $url and returns the all matches for the nonce regex or false if not found.
+	 *
+	 * @param Requests_Session $session The session to send the request from.
+	 * @param string $url The page URL.
+	 * @param string $regex The regex to use to find the nonce. It must have a single capture group.
+	 *
+	 * @return string|false The nonce if found, otherwise false.
+	 */
+	public static function findAllOnPage(\Requests_Session $session, $url, $regex = '/name="_wpnonce"\s+value="(.+?)"/')
+	{
+		//TODO: improve this to be more than just a dumb regex match
+		$nonceMatches = Page::findAll($session, $url, $regex);
+		if (!is_array($nonceMatches) || count($nonceMatches) < 2 || empty($nonceMatches[1])) {
+			return false;
+		}
+		$nonces = [];
+		foreach ($nonceMatches as $m) {
+			$nonces[] = $m[1];
+		}
+		return $nonces;
 	}
 }
