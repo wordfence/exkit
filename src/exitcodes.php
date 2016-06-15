1d0476d7320e01188a8765ad9971b493ce64f046
Panagiotis Vagenas <pan.vagenas@gmail.com>
2016-06-15 10:04:45 +0300

    Methods for easier exiting

--- src/exitcodes.php
+++ src/exitcodes.php
@@ -9,4 +9,79 @@ class ExitCodes
 	const EXIT_CODE_EXPLOIT_FAILED = 2;
 	const EXIT_CODE_FAILED_PRECONDITION = 3;
 	const EXIT_CODE_VALID_REQUEST_FAILED = 4;
+
+	/**
+	 * @param int $exitCode
+	 *
+	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
+	 * @since  0-dev
+	 */
+	public static function exitWith( $exitCode = self::EXIT_CODE_INFORMATIONAL_ONLY ) {
+		exit( $exitCode );
+	}
+
+	/**
+	 * @param string $msg A message to display before exiting
+	 *
+	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
+	 * @since  0-dev
+	 */
+	public function exitWithFailed( $msg = '' ) {
+		if ( $msg ) {
+			Cli::writeError( $msg );
+		}
+		self::exitWith( self::EXIT_CODE_EXPLOIT_FAILED );
+	}
+
+	/**
+	 * @param string $msg A message to display before exiting
+	 *
+	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
+	 * @since  0-dev
+	 */
+	public function exitWithSuccess( $msg = '' ) {
+		if ( $msg ) {
+			Cli::writeSuccess( $msg );
+		}
+		self::exitWith( self::EXIT_CODE_EXPLOIT_SUCCEEDED );
+	}
+
+	/**
+	 * @param string $msg A message to display before exiting
+	 *
+	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
+	 * @since  0-dev
+	 */
+	public function exitWithInformational( $msg = '' ) {
+		if ( $msg ) {
+			Cli::writeInfo( $msg );
+		}
+		self::exitWith( self::EXIT_CODE_INFORMATIONAL_ONLY );
+	}
+
+	/**
+	 * @param string $msg A message to display before exiting
+	 *
+	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
+	 * @since  0-dev
+	 */
+	public function exitWithFailedPrecondition( $msg = '' ) {
+		if ( $msg ) {
+			Cli::writeError( $msg );
+		}
+		self::exitWith( self::EXIT_CODE_FAILED_PRECONDITION );
+	}
+
+	/**
+	 * @param string $msg A message to display before exiting
+	 *
+	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
+	 * @since  0-dev
+	 */
+	public function exitWithFailedRequest( $msg = '' ) {
+		if ( $msg ) {
+			Cli::writeError( $msg );
+		}
+		self::exitWith( self::EXIT_CODE_VALID_REQUEST_FAILED );
+	}
 }
